6f4d7c6f5c15af31002f6f3ce22b9db40842b40d
wkoszolko <w.koszolko@gmail.com>
2015-12-06 21:16:32 +0100

    Add http response interceptor, which looking for date-strings (base on format) and create date object.
    Change-Id: Ib2bcdbfbc4ed77795edd174e8b29d9b56e0b00e5

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,5 +1,35 @@
 (function(angular) {
     angular.module("myApp.controllers", []);
     angular.module("myApp.services", []);
-    angular.module("myApp", ["ngResource", "ngMessages", "spring-data-rest", "myApp.controllers", "myApp.services"]);
+    var app = angular.module("myApp", ["ngResource", "ngMessages", "spring-data-rest", "myApp.controllers", "myApp.services"]);
+
+    app.config(["$httpProvider", function ($httpProvider) {
+        $httpProvider.defaults.transformResponse.push(function(responseData){
+            convertDateStringsToDates(responseData);
+            return responseData;
+        });
+    }]);
+
+    var regexDate = /^(\d{4}-\d{2}-\d{2})$/;
+
+    function convertDateStringsToDates(input) {
+        // Ignore things that aren't objects.
+        if (typeof input !== "object") return input;
+
+        for (var key in input) {
+            if (!input.hasOwnProperty(key)) continue;
+
+            var value = input[key];
+            var match;
+            if (typeof value === "string" && (match = value.match(regexDate))) {
+                // parse a date in yyyy-mm-dd format
+                var parts = match[0].split('-');
+                // new Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
+                input[key] = new Date(parts[0], parts[1]-1, parts[2]); // Note: months are 0-based
+            } else if (typeof value === "object") {
+                // Recurse into object
+                convertDateStringsToDates(value);
+            }
+        }
+    }
 }(angular));
