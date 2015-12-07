ab51c7b47b270dba50849686a3eaeac2f489c734
wkoszolko <w.koszolko@gmail.com>
2015-12-07 23:20:45 +0100

    Add saving date
    Change-Id: Ib9ef01cd3a71c63fb226685dcf4d796a83d825e7

--- src/main/resources/static/app/controllers.js
+++ src/main/resources/static/app/controllers.js
@@ -5,11 +5,12 @@
         });
 
         $scope.save = function(newRR) {
+            var today = new Date();
             new RR({
                 systolic: newRR.systolic,
                 diastolic: newRR.diastolic,
                 hr: newRR.hr,
-                date: null
+                date: buildDateString(today)
             }).save(function(rr) {
                     $scope.rrs.push(rr);
                     newRR = {
@@ -18,6 +19,15 @@
                         hr: null
                     };
             });
+        };
+
+        function buildDateString(date) {
+            //create date string in yyyy-mm-dd format
+            //months are 0-based
+            var day = date.getDate()<10 ? '0' + date.getDate() : date.getDate();
+            var month = date.getMonth()<9 ? '0' + (date.getMonth()+1) : (date.getMonth()+1);
+            var year = date.getFullYear();
+            return year + '-' + month + '-' + day;
         }
     };
 
